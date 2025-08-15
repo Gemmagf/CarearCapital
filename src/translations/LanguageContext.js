b3c066cebd4dfe20197f2161622ab98dd0d35ff6
Gemmagf <gemmagf@hotmail.es>
2025-08-15 13:27:57 +0200

    estrzcztira oer afegur uduines

---
+++ src/translations/LanguageContext.js
@@ -0,0 +1,17 @@
+eimport { createContext, useState, useContext } from "react";
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("catalan"); // Idioma per defecte
+
+  const changeLanguage = (lang) => setLanguage(lang);
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
