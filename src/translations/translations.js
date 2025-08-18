00acc7179505bdec3ccd86c07194dd3bc05ca9da
Gemmagf <gemmagf@hotmail.es>
2025-08-18 19:49:16 +0200

    versio masa minima del cv

---
+++ src/translations/translations.js
@@ -0,0 +1,34 @@
+const translations = {
+  ca: {
+    contactTitle: "Contacte",
+    contactText: "Per a col·laboracions o consultes, no dubtis en contactar-me.",
+    name: "Nom",
+    email: "Email",
+    message: "Missatge",
+    namePlaceholder: "El teu nom",
+    emailPlaceholder: "el.teu.email@exemple.com",
+    messagePlaceholder: "Escriu el teu missatge aquí...",
+    sendButton: "Enviar Missatge",
+    connectWithMe: "Connecta amb mi:",
+    skillsTitle: "Habilitats i Formació",
+    educationTitle: "Formació",
+    keySkillsTitle: "Habilitats Clau",
+  },
+  en: {
+    contactTitle: "Contact",
+    contactText: "For collaborations or inquiries, feel free to contact me.",
+    name: "Name",
+    email: "Email",
+    message: "Message",
+    namePlaceholder: "Your name",
+    emailPlaceholder: "your.email@example.com",
+    messagePlaceholder: "Write your message here...",
+    sendButton: "Send Message",
+    connectWithMe: "Connect with me:",
+    skillsTitle: "Skills & Education",
+    educationTitle: "Education",
+    keySkillsTitle: "Key Skills",
+  },
+};
+
+export default translations;
