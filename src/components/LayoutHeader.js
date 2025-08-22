fb740450576d5b5de8ca3b373179fa12aa15e0f2
Gemmagf <gemmagf@hotmail.es>
2025-08-22 10:51:53 +0200

    emoji llengues afegit

--- src/components/LayoutHeader.js
+++ src/components/LayoutHeader.js
@@ -9,14 +9,21 @@ const LayoutHeader = ({ setCurrentPage, setAppLanguage, translations }) => {
         <button onClick={() => setCurrentPage('cv')} className="text-gray-600 hover:text-black transition-colors font-medium">{translations.nav.skills}</button>
         <button onClick={() => setCurrentPage('personalProjects')} className="text-gray-600 hover:text-black transition-colors font-medium">{translations.nav.personalProjects}</button>
         <button onClick={() => setCurrentPage('contact')} className="text-gray-600 hover:text-black transition-colors font-medium">{translations.nav.contact}</button>
-        <select onChange={(e) => setAppLanguage(e.target.value)} className="ml-4 p-2 border border-gray-300 rounded-md text-gray-700 bg-white">
-          <option value="catalan">Català</option>
-          <option value="english">English</option>
-          <option value="german">Deutsch</option>
-          <option value="spanish">Castellà</option>
-          <option value="french">Français</option>
-          <option value="italian">Italiano</option>
-        </select>
+        
+        <div className="flex items-center space-x-2 ml-4">
+          <span role="img" aria-label="Idioma" className="text-lg">🌐</span>
+          <select
+            onChange={(e) => setAppLanguage(e.target.value)}
+            className="p-2 border border-gray-300 rounded-md text-gray-700 bg-white"
+          >
+            <option value="catalan">Català</option>
+            <option value="english">English</option>
+            <option value="german">Deutsch</option>
+            <option value="spanish">Castellà</option>
+            <option value="french">Français</option>
+            <option value="italian">Italiano</option>
+          </select>
+        </div>
       </nav>
     </header>
   );
